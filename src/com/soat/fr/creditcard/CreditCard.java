36091ea8c27630895734fdbb86bf2c3e3ef88600
so@t <dirtech@soat.fr>
2011-08-08 16:54:06 +0200

    suppression de fichiers

--- src/com/soat/fr/creditcard/CreditCard.java
+++
@@ -1,9 +0,0 @@
-package com.soat.fr.creditcard;
-
-import com.soat.fr.creditcard.internal.CreditCardSplitting;
-
-public interface CreditCard {
-
-	public boolean isCreditCardValid(CreditCardSplitting ccs);
-
-}
